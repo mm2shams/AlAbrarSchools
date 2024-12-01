f02ed045dbcb4b446c4fc021aca0761158801399
mm2shams <130877904+mm2shams@users.noreply.github.com>
2024-12-01 23:12:18 +0300

    Add files via upload

---
+++ EG Staff/script.js
@@ -0,0 +1,35 @@
+
+const listItems = document.querySelectorAll('#List li');
+const messageDiv = document.getElementById('message');
+
+
+listItems.forEach(item => {
+  item.addEventListener('click', function() {
+
+    navigator.clipboard.writeText(this.textContent)
+      .then(() => {
+
+        messageDiv.textContent = 'تم نسخ: ' + this.textContent;
+        messageDiv.style.display = 'block';
+        setTimeout(() => {
+          messageDiv.style.display = 'none'; 
+        }, 3000);
+      })
+      .catch(err => {
+        console.error('فشل النسخ: ', err);
+      });
+  });
+});
+
+
+function redirectToLink(selectElement) {
+  const selectedValue = selectElement.value;
+  if (selectedValue) {
+      window.location.href = selectedValue; // Redirect to the selected link
+  }
+}
+
+function toggleMenu() {
+  const mobileMenu = document.getElementById('mobile-menu');
+  mobileMenu.classList.toggle('active');
+}
